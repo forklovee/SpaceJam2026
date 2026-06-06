2aa1feb74c313388a6ab8fb2ac42b777018e3451
Piotrek Orlof <piotrorlof.po@gmail.com>
2026-06-06 21:39:47 +0200

    Ship stats

--- Ship/ship.gd
+++ Ship/ship.gd
@@ -1,5 +1,19 @@
 class_name Ship extends RigidBody3D
 
+signal health_changed(ship: Ship)
+signal storage_changed(ship: Ship)
+signal fuel_changed(ship: Ship)
+
+@export var max_health: int = 100
+var health: int = max_health
+@export var max_storage: int = 10
+var storage: int = 0
+@export var max_fuel: int = 100
+var fuel: int = max_fuel
+
+@export_group("Movement")
+@export var speed: float = 20.0
+
 var steering_direction: Vector2
 var movement_direction: Vector2
 
@@ -37,3 +51,37 @@ func steer(input_direction: Vector2):
 	steering_direction = steering_direction.lerp(
 		pre_target_direction, 0.1
 	)
+
+
+func damage(instigator: Node3D, damage: int):
+	health_changed.emit(self)
+
+func heal(instigator: Node3D, value: int):
+	health_changed.emit(instigator)
+
+func is_storage_full() -> bool:
+	return storage == max_storage
+
+func can_gather_crystals(amount: int) -> bool:
+	return storage + amount <= max_storage
+
+#TODO: define crystal piece
+func gather_crystal(crystal_piece: Node3D, amount: int):
+	if can_gather_crystals(amount):
+		return
+	
+	if !crystal_piece.is_inside_tree():
+		printerr(self, ": gather_crystal: crystal piece on inside tree!")
+		return
+	
+	storage = clamp(storage+amount, 0, max_storage)
+	storage_changed.emit(self)
+
+func can_use_fuel(value: int) -> bool:
+	return fuel - value >= 0
+
+func use_fuel(value: int):
+	if !can_use_fuel(value):
+		return
+	fuel = clamp(fuel-value, 0, max_fuel)
+	fuel_changed.emit(self)
