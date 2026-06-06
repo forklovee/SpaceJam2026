35bd2db6bfad4635a2d4d4d4a5e085cb43cc177d
MichalBiesek <michal.biesek@itsilesia.com>
2026-06-07 00:01:19 +0200

    score text
    score text

--- MainMenu/win.gd
+++ MainMenu/win.gd
@@ -1,7 +1,10 @@
 extends Node
 
+func _ready() -> void:
+	$Label2.text="(your score X enemy score Y)".replace("X",Game.player_score).replace("Y",Game.enemy_score)
 
 func _on_button_pressed() -> void:
 	Game.player_score=0
 	Game.enemy_score=0
+	
 	get_tree().change_scene_to_file("res://main.tscn")
