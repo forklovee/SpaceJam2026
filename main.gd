e39c1d1b7de2d936a9fde448ada3cb2fb2e35615
Piotrek Orlof <piotrorlof.po@gmail.com>
2026-06-06 21:03:48 +0200

    Fix Level

--- main.gd
+++ main.gd
@@ -4,6 +4,5 @@ extends Node3D
 @export var debug_level: PackedScene
 
 func _ready() -> void:
-	assert(debug_level!=null)
 	Game.open_level(debug_level)
 	Game.pc.spawn(Transform3D.IDENTITY, debug_player_ship)
