250d31bff84c5a24a2026260073c6b19699ae12e
Piotrek Orlof <piotrorlof.po@gmail.com>
2026-06-07 16:44:05 +0200

    Briefing

--- main.gd
+++ main.gd
@@ -3,9 +3,13 @@ extends Node3D
 @export var debug_player_ship: PackedScene
 @export var debug_level: PackedScene
 
-func _ready() -> void:
+func start() -> void:
 	var level := Game.open_level(debug_level)
 	if !level.is_node_ready():
 		await level.ready
 	
 	Game.pc.spawn(level.get_player_spawn_transform(), debug_player_ship)
+
+
+func _on_briefieng_briefing_end() -> void:
+	start()
