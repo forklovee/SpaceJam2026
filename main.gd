27e076423ec2bd2f1055a0d3d1241dd488a108b2
MichalBiesek <michal.biesek@itsilesia.com>
2026-06-07 16:41:07 +0200

    alpha
    alpha

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node3D
 @export var debug_level: PackedScene
 
 func _ready() -> void:
-	var level := Game.open_level(debug_level)
+	var level = Game.open_level(debug_level)
 	if !level.is_node_ready():
 		await level.ready
 	
